ece3a86b82e6045af71a74ebb7ca0c68f9c80680
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 20:11:34 +0500

    Add files via upload

---
+++ Q.21.ts
@@ -0,0 +1,5 @@
+// Q21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person:{name:string, fname:string, age:number} = {name: "Sufiyan", fname: "Amir", age:17 }
+
+console.log(person);
